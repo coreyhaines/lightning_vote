141cd17eef0f2f29130e2a849faf06e1e87b16a8
Corey Haines <coreyhaines@gmail.com>
2014-12-20 15:37:43 -0600

    Put in route for Talks#index that was accidentally left off when route constraints were added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :talks, only: [:new, :create]
+  resources :talks, only: [:index, :new, :create]
   resources :events, only: [:new, :create, :edit, :update]
 end

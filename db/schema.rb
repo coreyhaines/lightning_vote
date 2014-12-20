38d46330cf3700990cd61ce1ee339973f7b52ba5
Corey Haines <coreyhaines@gmail.com>
2014-12-20 15:37:54 -0600

    Add db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,19 @@
 
 ActiveRecord::Schema.define(version: 20141220163243) do
 
-  create_table "talks", force: :cascade do |t|
-    t.string   "topic"
-    t.string   "email"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "events", force: :cascade do |t|
     t.string   "title",       null: false
     t.text     "description"
     t.date     "date"
     t.text     "start_time"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "talks", force: :cascade do |t|
+    t.string   "topic"
+    t.string   "email"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end

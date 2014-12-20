6fd5fd9728570cf66a2e59b3eee1271856b07a2c
Gale Van Rossem <galeforcevr@gmail.com>
2014-12-20 11:09:35 -0600

    Basic new/create cycle for an Event with a title field

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,15 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    Event.create(event_creation_params)
+    redirect_to new_event_url
+  end
+
+private
+  def event_creation_params
+    params.require(:event).permit(:title)
+  end
+end

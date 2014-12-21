fd736aab2caf81af5db8f53881c78caedb1fc104
Corey Haines <coreyhaines@gmail.com>
2014-12-21 09:47:09 -0600

    Remove old non-namespaced routes for talks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :talks, only: [:index, :new, :create]
   namespace :admin do
     resources :events, only: [:index, :new, :create, :edit, :update]
   end
+
   namespace :public do
     resources :events, only: [] do
       resources :talks, only: [:index, :create]

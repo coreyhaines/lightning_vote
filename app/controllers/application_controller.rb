defbb1054b3701f5d77b1245c07f7e009b40a80c
Corey Haines <coreyhaines@gmail.com>
2014-12-21 15:54:09 -0600

    Remove unnecessary comment about current_user, now that we have it integrated and working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   private
   def current_user
-    # this is here because rspec doubles are verifying. This will eventually be implemented with the actual logged in user when we have logged in users
     User.find(session[:current_user_id])
   end
 end

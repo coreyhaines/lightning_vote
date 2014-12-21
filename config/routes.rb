14fa2d56d999dd799d657cf4eb673ebaa693bed0
Corey Haines <coreyhaines@gmail.com>
2014-12-21 09:05:24 -0600

    Public viewing of talks for any event. Currently doesn't show talks, but does support the view with the event name.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :events, only: [:index, :new, :create, :edit, :update]
   end
+  namespace :public do
+    resources :events, only: [] do
+      resources :talks, only: [:index]
+    end
+  end
 end

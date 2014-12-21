dd190351612a8a8556919221e949e7ef50b6bb61
Corey Haines <coreyhaines@gmail.com>
2014-12-21 10:43:32 -0600

    Public event page shows event information + list of talks

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   validates :title, presence: true
   has_many :talks
+  belongs_to :administrator, class: User
 
   def submit_talk(params)
     talks.create params
--- spec/requests/public/viewing_an_event_spec.rb
+++ spec/requests/public/viewing_an_event_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Publicly viewing an event", type: :request do
   describe "GET /events/:id" do
-    let(:event) { Event.create! title: "Example Event" }
+    let(:administrator) { User.create! name: "Corey" }
+    let(:event) { administrator.create_event title: "Example Event" }
     it "Shows the basic event information" do
       get "/events/#{event.id}"
 
       expect(response).to have_http_status(200)
       expect(response.body).to include("Example Event")
+      expect(response.body).to include("Organizer: Corey")
     end
 
     it "Shows the list of talks associated with the event" do

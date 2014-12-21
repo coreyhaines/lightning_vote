99c965009bfbab07b20517f97e6e0391bf954d87
Corey Haines <coreyhaines@gmail.com>
2014-12-21 10:39:33 -0600

    Event show page now shows talks associated with event

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -2,6 +2,7 @@ module Public
   class EventsController < ApplicationController
     def show
       @event = Event.find(params[:id])
+      @talks = @event.talks
     end
   end
 end
--- spec/requests/public/viewing_an_event_spec.rb
+++ spec/requests/public/viewing_an_event_spec.rb
@@ -9,5 +9,15 @@ RSpec.describe "Publicly viewing an event", type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include("Example Event")
     end
+
+    it "Shows the list of talks associated with the event" do
+      event.submit_talk(topic: "Example Talk 1", email: "corey@example.com")
+      event.submit_talk(topic: "Example Talk 2", email: "corey@example.com")
+
+      get "/events/#{event.id}"
+
+      expect(response.body).to include("Example Talk 1")
+      expect(response.body).to include("Example Talk 2")
+    end
   end
 end

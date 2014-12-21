2f6ce5d7a78b2eb6b9ac3ef09e1e023c46a053e5
Shreya Patel <shreya317@gmail.com>
2014-12-21 17:46:37 -0600

    Removed unnecessary EventTalk routes and files

--- app/controllers/admin/event_talks_controller.rb
+++
@@ -1,4 +0,0 @@
-module Admin
-	class EventTalksController < ApplicationController
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :events, only: [:index, :new, :create, :edit, :update] do
-      resources :eventtalk, only: [:new, :create, :edit, :update]
-    end
+    resources :events, only: [:index, :new, :create, :edit, :update]
   end
 
   resource :current_user, :controller => "current_user", only: [:edit, :update]

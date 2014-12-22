47d8e818bd1ae17e443b347ba20aadd0c477e8a2
Shreya Patel <shreya317@gmail.com>
2014-12-21 18:05:56 -0600

    Added index to events

--- db/migrate/20141221170400_create_event_talks.rb
+++ db/migrate/20141221170400_create_event_talks.rb
@@ -2,7 +2,7 @@ class CreateEventTalks < ActiveRecord::Migration
   def change
     create_table :event_talks do |t|
       t.references :talk, index: true
-      t.references :event
+      t.references :event, index:true
       t.timestamps null: false
     end
   end

9d9b8d23ac25cc28ae6bd4fd05534239fbc74202
Corey Haines <coreyhaines@gmail.com>
2014-12-22 19:02:52 -0600

    Basic page for submitting a talk

--- spec/requests/public/creating_a_talk_spec.rb
+++ spec/requests/public/creating_a_talk_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "Creating a Talk", type: :request do
   describe "POST /events/:event_id/talks" do
     it "creates a talk attached to the event" do
       event = Event.create!(title: "Example Event")
-      event_talk = EventTalk.create!(event: event)
+      get new_public_event_talk_path(event)
+      expect(response).to have_http_status(200)
+
       post public_event_talks_path(event), event_id: event.id, talks: { topic: "Example Topic", email: "corey@example.com" }
       follow_redirect!
       expect(response.body).to match("Example Topic")

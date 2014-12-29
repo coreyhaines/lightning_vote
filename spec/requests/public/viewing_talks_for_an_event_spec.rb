743708d379429c153428faa6b6e1c201dfb3ea57
Corey Haines <coreyhaines@gmail.com>
2014-12-29 15:37:22 -0600

    Fix tests that broke after adding authentication

--- spec/requests/public/viewing_talks_for_an_event_spec.rb
+++ spec/requests/public/viewing_talks_for_an_event_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Creating a Talk", type: :request do
+  let(:organizer) { User.create username: "corey" }
   context "With no talks accepted for the event" do
     it "Shows a message about no talks registered yet" do
-      event = Event.create title: "Example Event"
+      event = organizer.create_event title: "Example Event"
 
-      get public_event_talks_path(event)
+      get event_path(event)
 
       expect(response).to have_http_status(200)
       expect(response.body).to match("Example Event")
@@ -14,12 +15,12 @@ RSpec.describe "Creating a Talk", type: :request do
 
   context "With talks accepted for the event" do
     it "Shows the talks" do
-      event = Event.create title: "Example Event"
+      event = organizer.create_event title: "Example Event"
       event.create_event_talk
       talk1 = event.submit_talk topic: "Example Topic 1", email: "corey@example.com"
       talk2 = event.submit_talk topic: "Example Topic 2", email: "corey@example.com"
 
-      get public_event_talks_path(event)
+      get event_path(event)
 
       expect(response.body).to match(talk1.topic)
       expect(response.body).to match(talk2.topic)

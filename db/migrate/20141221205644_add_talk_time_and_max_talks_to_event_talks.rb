1c7965ac0f0c38ae69e204cc4c5f7074f59462e2
Corey Haines <coreyhaines@gmail.com>
2014-12-31 14:02:58 -0600

    Clean up migrations, merge some that were result of dev changes

--- db/migrate/20141221205644_add_talk_time_and_max_talks_to_event_talks.rb
+++
@@ -1,6 +0,0 @@
-class AddTalkTimeAndMaxTalksToEventTalks < ActiveRecord::Migration
-  def change
-    add_column :event_talks, :talk_time, :integer
-    add_column :event_talks, :talk_slots, :integer
-  end
-end
